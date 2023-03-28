f3ca763cb84fccec4239b62a5ea8b4c9b2cc772a
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-28 12:39:39 +0100

    Poroduct Model

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -2,9 +2,42 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = mongoose.Schema(
-    {
-        
-    }
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a product Name'],
+      unique: true
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    category_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: [true, 'Product must belong to a category']
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'Please add a product Description'],
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: [true, 'Product must have a price'],
+      maxlength: 32
+
+    },
+    image: {
+      type: String,
+      default: null
+    },
+
+  },
+  { timestamps: true }
 )
 
 module.exports = mongoose.model('Product', productSchema)
